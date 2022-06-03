a9e5be88eafab5306415d6460df365556a2673c4
Talgat <tairovtalgat@gmail.com>
2022-06-03 19:03:42 +0600

    remove css colors vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,13 @@ module.exports = {
         allowImportExportEverywhere: true,
         ecmaVersion: 13
     },
+    plugins: ["import"],
     rules: {
         semi: ["error", "always"],
         quotes: ["warn", "double"],
         "comma-dangle": ["warn"],
         "no-multiple-empty-lines": "off",
-        indent: ["warn", 4, {SwitchCase: 1}]
+        indent: ["warn", 4, {SwitchCase: 1}],
+        "import/no-relative-parent-imports": "error"
     }
 };
